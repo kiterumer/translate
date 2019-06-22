7bafea0a2dc34be912caaec1dfd8e8580eb4d5a0
kiterumer <1404646092@qq.com>
2019-06-22 11:51:23 +0800

    解决request合法域名问题

--- pages/change/change.js
+++ pages/change/change.js
@@ -10,7 +10,7 @@ Page({
     this.setData({ curLang: app.globalData.curLang })
   },
   onTapItem: function(e) {
-    let langObj = e.currentTarget.dataset
+    let langObj = e.currentTarget.dataset  //自定义属性
     wx.setStorageSync('language', langObj)
     this.setData({'curLang': langObj})
     app.globalData.curLang = langObj
